07b8a8fd9a9ef87e98006277b20badacb0c5581b
Rails Udemy Course <ericzorndesigns@gmail.com>
2018-06-17 03:42:24 +0000

    Before Action Articles Controller Set and UI Login System Update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   
+  before_action :set_user, only: [:edit, :update, :show]
   
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -23,13 +24,12 @@ class UsersController < ApplicationController
   
   
   def edit
-    @user = User.find(params[:id])
+    
   end
   
   
   
   def update
-    @user = User.find(params[:id])
     
     if @user.update(user_params)
       flash[:success] = "Your Account Was Updated Successfully"
@@ -42,16 +42,18 @@ class UsersController < ApplicationController
   
   
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
   
   private
+  
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
   
-  
+  def set_user
+    @user = User.find(params[:id])
+  end
   
 end
