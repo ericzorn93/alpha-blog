d617ab95da9fcd1604893d48af5c45659fb6fa90
Rails Udemy Course <ericzorndesigns@gmail.com>
2018-06-23 17:44:05 +0000

    Cateogries Model test

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,7 @@ class ArticlesController < ApplicationController
   before_action :required_user, except: [:index, :show]
   before_action :require_same_user, only: [:edit, :update, :destroy]
   
+  
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
